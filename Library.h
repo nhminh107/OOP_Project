a069d7754d140646f4aa5ccf0d280aca80f4484b
nhminh107 <24120381@student.hcmus.edu.vn>
2025-12-09 20:05:38 +0700

    Add project files.

---
+++ Library.h
@@ -0,0 +1,33 @@
+#ifndef _LIB_H_
+#define _LIB_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <unordered_map>
+#include <stack>
+#include <locale>
+#include <codecvt>
+#include "rapidxml.hpp"
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN 
+// Windows Header Files
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+using namespace std;
+using namespace rapidxml;
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+
+
+#endif 
