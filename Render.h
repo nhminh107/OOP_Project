68036d34eea46371114f74037bcdf5b986371b82
nhminh107 <24120381@student.hcmus.edu.vn>
2025-12-09 21:00:41 +0700

    Commit Render, Parser,  Image

---
+++ Render.h
@@ -0,0 +1,17 @@
+#ifndef _RENDERER_H_
+#define _RENDERER_H_
+
+class renderer {
+public:
+	void renderFigure(Graphics&, group*);
+
+	void drawRectangle(Graphics&, rectangle*);
+	void drawEllipse(Graphics&, ellipse*);
+	void drawLine(Graphics&, line*);
+	void drawPolygon(Graphics&, polygon*);
+	void drawPolyline(Graphics&, polyline*);
+	void drawText(Graphics&, text*);
+	void drawPath(Graphics&, path*);
+};
+
+#endif
